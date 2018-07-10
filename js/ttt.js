dc4d833fcd0325e35909d7031af4dc2d029c8aa9
Gramkraxor <Gramkraxor@users.noreply.github.com>
2018-07-09 21:10:11 -0500

    2018-07-09.1
    Updated js/ttt to version 2.0.0.
    Removed optional tags from templates.

--- js/ttt.js
+++ js/ttt.js
@@ -1,153 +1,217 @@
 /*
+ * js/ttt v2.0.0
  * Ultimate Tic Tac Toe game
  * (c) 2018 Gramkraxor
  */
 
-const ttt = {}; // namespace object
+const NAME = "Ultimate Tic Tac Toe";
+const ID = "js/ttt";
+const VERSION = "2.0.0";
+const AUTHORS = [ "Gramkraxor" ];
+const YEAR = 2018;
 
-ttt.NAME = "Ultimate Tic Tac Toe";
-ttt.AUTHORS = [ "Gramkraxor" ];
-ttt.YEAR = 2018;
+const players = [ "x", "o" ];
+const open = "_";
 
-// superior string replacement function
-ttt.repl = function(s, o, n) {
-	return s.split(o).join(n);
-}
+const colorClasses = [ "dark", "pale" ];
 
-// array equals?
-ttt.eq = function(a, b) {
-	return JSON.stringify(a) == JSON.stringify(b);
-}
+const boardSizes = [ 2, 3, 4, 5 ];
+const defaultBoardSizesIndex = 1;
+let boardSizesIndex = 1;
 
-// array contains?
-ttt.has = function(arr, x) {
-	for (let i = 0; i < arr.length; i++) {
-		if (ttt.eq(arr[i], x)) return true;
-	}
-	return false;
-}
+let justReset = false;
 
-// create a seperate copy of array
-ttt.copy = function(arr) {
+let victor;
+let gboard; // array of 3^2 large cells
+let mboard; // array of 3^4 small cells
+
+let posPos; // array of possible next moves
+let turnsTaken;
+let beenUndone;
+let prevData;
+
+$(() => {
+	$("body")
+		.append($("<div>", { id: "wrapper" })
+			.append($("<div>", { id: "board" }))
+			.append($("<div>", { id: "buttons" })
+				.append($("<div>", { id: "undo",    text:  "Undo",   click: onUndo    }))
+				.append($("<div>", { id: "reset",   text:  "Reset",  click: onReset   }))
+				.append($("<div>", { id: "resize",                   click: onResize  }))
+				.append($("<div>", { id: "recolor", html:  "&nbsp;", click: onRecolor }))
+			)
+		)
+		.append($("<div>", { id: "v", text: "v" + VERSION }))
+		.append($("<div>", { id: "gk", text: "\u00A9 " + AUTHORS[0] }));
+	onStart();
+});
+
+
+function copy(arr) {
 	return JSON.parse(JSON.stringify(arr));
 }
 
-// class names/array values
-ttt.G = "g";
-ttt.M = "m";
-ttt.X = "x";
-ttt.O = "o";
-ttt.open = "open";
-ttt.cat  = "cat";
-ttt.Xish = "x-ish";
-ttt.Oish = "o-ish";
-ttt.nope = "nope";
-
-ttt.cols;
-ttt.rows;
-ttt.connect; // cells to connect to conquer
-
-ttt.gboard; // array of 3^2 large cells
-ttt.mboard; // array of 3^4 small cells
-
-ttt.turn; // whose turn it is
-ttt.posPos; // array of possible next moves
-ttt.turnsTaken; // number of turns taken in the current game
-
-// remember the last turn's data to allow the undo function
-ttt.prevGboard;
-ttt.prevMboard;
-ttt.prevPosPos;
-
-ttt.undone; // whether the previous move has been undone
-
-ttt.sizes = [ 2, 3, 4, 5 ]; // standard board sizes
-ttt.sizeIndex = 1; // index of scrolling through ttt.sizes
-
-// generate cell name
-ttt.getName = function(gx, gy, mx, my) {
-	let r = ttt.G + gx + gy;
-	if (!isNaN(my)) r += ttt.M + mx + my;
-	return r;
-}
-
-// select cell
-ttt.getCell = function(gx, gy, mx, my) {
-	return $("#" + ttt.getName(gx, gy, mx, my));
+function color(index) {
+	if (!isNaN(index)) {
+		color.index = index;
+	} else if (!color.index) {
+		color.index = 0;
+	}
+	color.index %= 4;
+	$("body")
+		.toggleClass(colorClasses[0], (color.index + 3) % 4 < 2)
+		.toggleClass(colorClasses[1], color.index > 1);
+	try {
+		storage(sessionStorage, ID + "/color", color.index);
+	} catch (e) {
+	}
 }
 
 // label a cell in the array and in the DOM
-ttt.draw = function(p, gx, gy, mx, my) {
-	if (p == ttt.open) return;
-	ttt.getCell(gx, gy, mx, my).addClass(p);
+function draw(p, gx, gy, mx, my) {
+	if (p === open) return;
+	getCell(gx, gy, mx, my).addClass(p);
 	if (!isNaN(my)) {
-		ttt.mboard[gx][gy][mx][my] = p;
+		mboard[gx][gy][mx][my] = p;
+	} else if (!isNaN(gy)) {
+		gboard[gx][gy] = p;
 	} else {
-		ttt.gboard[gx][gy] = p;
+		victor = p;
 	}
 }
 
-// test for a win
-ttt.bingo = function(p, gx, gy) {
-	let board = (isNaN(gy))? ttt.gboard : ttt.mboard[gx][gy];
+// re-label all items
+function drawAll() {
+	draw(victor);
+	for (let gx = 0; gx < getBoardSize(); gx++) {
+		for (let gy = 0; gy < getBoardSize(); gy++) {
+			draw(gboard[gx][gy], gx, gy);
+			for (let mx = 0; mx < getBoardSize(); mx++) {
+				for (let my = 0; my < getBoardSize(); my++) {
+					draw(mboard[gx][gy][mx][my], gx, gy, mx, my);
+				}
+			}
+		}
+	}
+}
 
-	// easter egg
-	if (gx == 1 && gy == 1) {
-		let bigX = [
-			[ttt.X,    ttt.open, ttt.X   ],
-			[ttt.open, ttt.X,    ttt.open],
-			[ttt.X,    ttt.open, ttt.X   ]
-		];
-		if (ttt.eq(board, bigX)) {
-			$("body").addClass("dark");
-		}
+// add hover-higlight class for possible next moves
+function drawish() {
+	for (let i = 0; i < posPos.length; i++) {
+		getCell(posPos[i][0], posPos[i][1]).addClass(getWhoseTurn() + "-ish");
 	}
+	if (turnsTaken === 0) $(".g").addClass("nope");
+}
 
-	// check for horizontal wins
-	H0: for (let x0 = 0; x0 < ttt.cols - ttt.connect + 1; x0++) {
-		V: for (let y = 0; y < ttt.rows; y++) {
-			H: for (let x = x0; x < ttt.cols; x++) {
-				if (board[x][y] != p) continue V;
-			}
-			return true;
+function eq(a, b) {
+	return JSON.stringify(a) === JSON.stringify(b);
+}
+
+function exportData(withPrevData) {
+	try {
+		let data = getData();
+		if (withPrevData) data.prevData = prevData;
+		storage(sessionStorage, ID + "/data", data);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+// label every open sopt as a possible next move
+function freeMove() {
+	posPos = [];
+	for (let gx = 0; gx < getBoardSize(); gx++) {
+		for (let gy = 0; gy < getBoardSize(); gy++) {
+			if (gboard[gx][gy] === open) posPos.push([gx, gy]);
 		}
 	}
-	// check for vertical wins
-	V0: for (let y0 = 0; y0 < ttt.rows - ttt.connect + 1; y0++) {
-		H: for (let x = 0; x < ttt.cols; x++) {
-			V: for (let y = y0; y < ttt.rows; y++) {
-				if (board[x][y] != p) continue H;
-			}
-			return true;
-		}
+}
+
+function getBoardSize() {
+	return boardSizes[boardSizesIndex];
+}
+
+function getCell(gx, gy, mx, my) {
+	if (isNaN(gx)) {
+		return $("#board");
+	} else {
+		return $("#" + getName(gx, gy, mx, my));
 	}
-	// check for positive diagonal ttt.victory
-	H0: for (let x0 = 0; x0 < ttt.cols - ttt.connect + 1; x0++) {
-		V0: for (let y0 = 0; y0 < ttt.rows - ttt.connect + 1; y0++) {
-			I: for (let i = 0; i < ttt.rows - x0 && i < ttt.cols - y0; i++) {
-					if (board[x0 + i][y0 + i] != p) continue V0;
-			}
-			return true;
+}
+
+function getData() {
+	return {
+		victor: victor,
+		gboard: copy(gboard),
+		mboard: copy(mboard),
+		posPos: copy(posPos),
+		boardSizesIndex: boardSizesIndex,
+		turnsTaken: turnsTaken,
+		justReset: justReset
+	};
+}
+
+// generate cell name
+function getName(gx, gy, mx, my) {
+	let r = "g" + gx + "-" + gy;
+	if (!isNaN(my)) r += "m" + mx + "-" + my;
+	return r;
+}
+
+function getWhoseTurn(turns) {
+	if (isNaN(turns)) turns = turnsTaken;
+	return players[turns % players.length];
+}
+
+function gotBingo(p, gx, gy) {
+	let board = (isNaN(gx) || isNaN(gy))? gboard : mboard[gx][gy];
+
+	let dexter   = true;
+	let sinister = true;
+	for (let i = 0; i < getBoardSize(); i++) {
+		let thisCol = true;
+		let thisRow = true;
+		for (let j = 0; j < getBoardSize(); j++) {
+			if (board[i][j] != p) thisCol = false;
+			if (board[j][i] != p) thisRow = false;
 		}
+		if (thisCol || thisRow) return true;
+		if (board[i][i]                      != p) dexter   = false;
+		if (board[i][getBoardSize() - 1 - i] != p) sinister = false;
 	}
-	// check for negative diagonal ttt.victory
-	H0: for (let x0 = 0; x0 < ttt.cols - ttt.connect + 1; x0++) {
-		V0: for (let y0 = ttt.rows - 1; y0 >= ttt.connect - 1; y0--) {
-			I: for (let i = 0; i < ttt.connect; i++) {
-					if (board[x0 + i][y0 - i] != p) continue V0;
-			}
-			return true;
-		}
+	return dexter || sinister;
+}
+
+// array contains?
+function has(arr, x) {
+	for (let i = 0; i < arr.length; i++) {
+		if (eq(arr[i], x)) return true;
 	}
 	return false;
 }
 
-// test for a tie
-ttt.catsGame = function(gx, gy) {
-	let board = (isNaN(gy))? ttt.gboard : ttt.mboard[gx][gy];
-	for (let x = 0; x < ttt.cols; x++) {
-		for (let y = 0; y < ttt.rows; y++) {
-			if (board[x][y] == ttt.open) {
+function importData() {
+	let data = storage(sessionStorage, ID + "/data");
+	if (!data) throw true;
+	boardSizesIndex = data.boardSizesIndex;
+	newDomBoard();
+	turnsTaken = data.turnsTaken;
+	victor = data.victor;
+	gboard = copy(data.gboard);
+	mboard = copy(data.mboard);
+	posPos = copy(data.posPos);
+	justReset = data.justReset;
+	prevData = data.prevData;
+	drawAll();
+	drawish();
+}
+
+function isCatsGame(gx, gy) {
+	let board = (isNaN(gy))? gboard : mboard[gx][gy];
+	for (let x = 0; x < getBoardSize(); x++) {
+		for (let y = 0; y < getBoardSize(); y++) {
+			if (board[x][y] === open) {
 				return false;
 			}
 		}
@@ -155,269 +219,224 @@ ttt.catsGame = function(gx, gy) {
 	return true;
 }
 
-// label every open sopt as a possible next move
-ttt.freebie = function() {
-	ttt.posPos = [];
-	for (let gx = 0; gx < ttt.cols; gx++) {
-		for (let gy = 0; gy < ttt.rows; gy++) {
-			if (ttt.gboard[gx][gy] == ttt.open) ttt.posPos.push([gx, gy]);
-		}
-	}
-}
-
-// start/restart entire game
-ttt.reset = function() {
-
-	ttt.gboard = [];
-	ttt.mboard = [];
-	for (let gx = 0; gx < ttt.cols; gx++) {
-		ttt.gboard.push([]);
-		ttt.mboard.push([]);
-		for (let gy = 0; gy < ttt.rows; gy++) {
-			ttt.gboard[gx].push(ttt.open);
-			ttt.mboard[gx].push([]);
-			for (let mx = 0; mx < ttt.cols; mx++) {
-				ttt.mboard[gx][gy].push([]);
-				for (let my = 0; my < ttt.rows; my++) {
-					ttt.mboard[gx][gy][mx].push(ttt.open);
+function newDataBoard() {
+	victor = open;
+	gboard = [];
+	mboard = [];
+	for (let gx = 0; gx < getBoardSize(); gx++) {
+		gboard.push([]);
+		mboard.push([]);
+		for (let gy = 0; gy < getBoardSize(); gy++) {
+			gboard[gx].push(open);
+			mboard[gx].push([]);
+			for (let mx = 0; mx < getBoardSize(); mx++) {
+				mboard[gx][gy].push([]);
+				for (let my = 0; my < getBoardSize(); my++) {
+					mboard[gx][gy][mx].push(open);
 				}
 			}
 		}
 	}
-
-	ttt.wipe();
-
-	ttt.turn = ttt.X;
-	ttt.turnsTaken = 0;
-	ttt.freebie();
-	ttt.drawish(true);
 }
 
-ttt.nextTurn = function() {
-	ttt.turn = (ttt.turn == ttt.O)? ttt.X : ttt.O;
-}
-
-// copy the previous board
-ttt.remember = function() {
-	ttt.prevGboard = ttt.copy(ttt.gboard);
-	ttt.prevMboard = ttt.copy(ttt.mboard);
-	ttt.prevPosPos = ttt.copy(ttt.posPos);
-}
-
-// undo one turn
-ttt.undo = function() {
-	if (ttt.undone || ttt.turnsTaken == 0) return;
-	ttt.undone = true;
-	ttt.turnsTaken--;
-	ttt.wipe();
-	ttt.gboard = ttt.copy(ttt.prevGboard);
-	ttt.mboard = ttt.copy(ttt.prevMboard);
-	ttt.posPos = ttt.copy(ttt.prevPosPos);
-	ttt.nextTurn();
-	for (let gx = 0; gx < ttt.cols; gx++) {
-		for (let gy = 0; gy < ttt.rows; gy++) {
-			ttt.draw(ttt.gboard[gx][gy], gx, gy);
-			for (let mx = 0; mx < ttt.cols; mx++) {
-				for (let my = 0; my < ttt.rows; my++) {
-					ttt.draw(ttt.mboard[gx][gy][mx][my], gx, gy, mx, my);
-				}
-			}
-		}
-	}
-	ttt.drawish(ttt.turnsTaken == 0);
-}
-
-// run through possible game sizes
-ttt.resize = function() {
-	ttt.sizeIndex++;
-	if (ttt.sizeIndex == ttt.sizes.length) ttt.sizeIndex = 0;
-	ttt.newBoard(ttt.sizes[ttt.sizeIndex]);
-}
-
-// add hover-higlight class for possible next moves
-ttt.drawish = function(nope) {
-	let ish = (ttt.turn == ttt.O)? ttt.Oish : ttt.Xish;
-	for (let i = 0; i < ttt.posPos.length; i++) ttt.getCell(ttt.posPos[i][0], ttt.posPos[i][1]).addClass(ish);
-	if (nope) $("." + ttt.G).addClass(ttt.nope);
-}
-
-// remove hover-higlight classes
-ttt.wipeish = function() {
-	$("." + ttt.G + ", ." + ttt.M).removeClass([ttt.Xish, ttt.Oish, ttt.nope].join(" "));
-}
-
-// remove changable classes
-ttt.wipe = function() {
-	$("#board, #board *").removeClass([ttt.X, ttt.O, ttt.cat, ttt.Xish, ttt.Oish, ttt.nope].join(" "));
-}
-
-// when a small square gets clicked
-ttt.onClick = function(gx, gy, mx, my) {
-	if (!(ttt.has(ttt.posPos, [gx, gy]) && ttt.mboard[gx][gy][mx][my] == ttt.open)) return;
-	ttt.remember();
-	ttt.turnsTaken++;
-	ttt.undone = false;
-	ttt.draw(ttt.turn, gx, gy, mx, my);
-
-	if (ttt.bingo(ttt.turn, gx, gy)) {
-		ttt.draw(ttt.turn, gx, gy);
-		if (ttt.bingo(ttt.turn)) {
-			ttt.victory(ttt.turn);
-			ttt.nextTurn();
-			return;
-		} else if (ttt.catsGame()) {
-			ttt.tie();
-			ttt.nextTurn();
-			return;
-		}
-	} else if (ttt.catsGame(gx, gy)) {
-		ttt.draw(ttt.cat, gx, gy);
-	}
-
-	ttt.nextTurn();
-	ttt.posPos = [];
-	if (ttt.gboard[mx][my] == ttt.open) {
-		ttt.posPos.push([mx, my]);
-	} else {
-		ttt.freebie();
-	}
-	ttt.wipeish();
-	ttt.drawish();
-}
-
-// somebody won
-ttt.victory = function(p) {
-	//console.log("VICTORY FOR " + ttt.turn.toUpperCase());
-	$("#board").addClass(p);
-}
-
-// nobody won
-ttt.tie = function() {
-	//console.log("IT\'S A TIE");
-	$("#board").addClass(ttt.cat);
-}
-
-// on load
-$(function() {
-
-	$("body")
-		.append($("<div/>")
-			.attr("id", "wrapper")
-			.append($("<div/>")
-				.attr("id", "board")
-			)
-			.append($("<div/>")
-				.attr("id", "buttons")
-				.append($("<div/>")
-					.attr("id", "undo")
-					.text("UNDO")
-					.click(function() { ttt.undo(); })
-				)
-				.append($("<div/>")
-					.attr("id", "reset")
-					.text("RESET")
-					.click(function() { ttt.reset(); })
-				)
-				.append($("<div/>")
-					.attr("id", "resize")
-					.click(function() { ttt.resize(); })
-				)
-				.append($("<div/>")
-					.attr("id", "darken")
-					.text("\u25A0")
-					.html("&nbsp;")
-					.click(function() {
-						if ($("body").hasClass("dark")) {
-							$("body").removeClass("dark");
-						} else {
-							$("body").addClass("dark");
-						}
-					})
-				)
-			)
-		)
-		.append($("<div/>")
-			.attr("id", "footer")
-			.text(["\u00A9", ttt.YEAR, ttt.AUTHORS[0]].join(" "))
-		)
-	;
-
-	ttt.newBoard();
-
-});
-
-ttt.newBoard = function(size) {
-
-	// empty the board for resizing
+// replace the DOM board
+function newDomBoard() {
 	$("#board").empty();
-	ttt.gboard = [];
-	ttt.mboard = [];
-
-	// default game size
-	if (isNaN(size)) size = 3;
-	ttt.cols = ttt.rows = ttt.connect = size;
-
-	$("#resize").text(size).append($("<sup/>").text("4"))
-
+	$("#resize").text(getBoardSize()).append($("<sup>").text("4"));
 	// make all the squares
-	for (let gy = 0; gy < ttt.rows; gy++) {
-
-		let ROW = "row";
-		let BOX = "box";
-		let GROW = ttt.G + ROW;
-		let GBOX = ttt.G + BOX;
-		$("#board").append($("<div/>") // starting with the row divs
-			.addClass(GROW)
-		);
-
-		for (let gx = 0; gx < ttt.cols; gx++) {
-
-			$("." + GROW).eq(gy).append($("<div/>")
-				.addClass(GBOX)
-				.append($("<div/>")
-					.attr("id", ttt.getName(gx, gy))
-					.addClass(ttt.G)
-				)
+	for (let gy = 0; gy < getBoardSize(); gy++) {
+		$("#board").append($("<div>", { class: "g-row" })); // starting with the row divs
+		for (let gx = 0; gx < getBoardSize(); gx++) {
+			$(".g-row").eq(gy).append($("<div>", { class: "g-box" })
+				.append($("<div>", { id: getName(gx, gy), class: "g" }))
 			);
-
 			// now make the smaller ones
-			for (let my = 0; my < ttt.rows; my++) {
-
-				let MROW = ttt.M + ROW;
-				let MBOX = ttt.M + BOX;
-				$("#" + ttt.getName(gx, gy)).append($("<div/>")
-					.addClass(MROW)
-				);
-
-				for (let mx = 0; mx < ttt.cols; mx++) {
-
-					$("#" + ttt.getName(gx, gy) + " ." + MROW).eq(my).append($("<div/>")
-						.addClass(MBOX)
-						.append($("<div/>")
-							.attr("id", ttt.getName(gx, gy, mx, my))
-							.addClass(ttt.M)
-							.click(function() {
-								ttt.onClick(gx, gy, mx, my);
-							})
-						)
+			for (let my = 0; my < getBoardSize(); my++) {
+				$("#" + getName(gx, gy)).append($("<div>", { class: "m-row" }));
+				for (let mx = 0; mx < getBoardSize(); mx++) {
+					$("#" + getName(gx, gy) + " .m-row").eq(my).append($("<div>", { class: "m-box" })
+						.append($("<div>", {
+							id: getName(gx, gy, mx, my),
+							class: "m",
+							click: () => onClick(gx, gy, mx, my)
+						}))
 					);
 				}
 			}
 		}
 	}
-
-	ttt.reset(); // begin!
-
 }
 
-ttt.show = function(arr) {
-	let r = "";
-	for (let gy = 0; gy < arr[0].length; gy++) {
-		for (let gx = 0; gx < arr.length; gx++) {
-			r += (arr[gx][gy] == ttt.open)? "_" : arr[gx][gy];
-			r += " ";
+// when a small square gets clicked
+function onClick(gx, gy, mx, my) {
+	if (!has(posPos, [gx, gy] || mboard[gx][gy][mx][my] !== open)) return;
+	prevData = getData();
+	let whoseTurn = getWhoseTurn();
+	justReset = false;
+	beenUndone = false;
+	turnsTaken++;
+	draw(whoseTurn, gx, gy, mx, my);
+
+	if (gotBingo(whoseTurn, gx, gy)) {
+		draw(whoseTurn, gx, gy);
+		let bingo = gotBingo(whoseTurn);
+		if (bingo || isCatsGame()) {
+			if (bingo) {
+				draw(whoseTurn);
+			} else {
+				draw("cat");
+			}
+			posPos = [];
+			exportData(true);
+			return;
 		}
-		if (gy < arr[0].length - 1) r += "\n";
+	} else if (isCatsGame(gx, gy)) {
+		draw("cat", gx, gy);
 	}
+
+	posPos = [];
+	if (gboard[mx][my] === open) {
+		posPos.push([mx, my]);
+	} else {
+		freeMove();
+	}
+	exportData(true);
+	wipeish();
+	drawish();
+}
+
+function onRecolor() {
+	color.index++;
+	color();
+}
+
+function onReset() {
+	if (!justReset) prevData = getData();
+	justReset = true;
+	beenUndone = false;
+	newDataBoard();
+	startNewGame();
+	exportData(true);
+}
+
+function onResize() {
+	if (!justReset) prevData = getData();
+	justReset = true;
+	beenUndone = false;
+	boardSizesIndex++;
+	boardSizesIndex %= boardSizes.length;
+	newDomBoard();
+	newDataBoard();
+	startNewGame();
+	exportData(true);
+}
+
+function onStart() {
+	try {
+		color(storage(sessionStorage, ID + "/color"));
+	} catch (e) {
+		color();
+	}
+	try {
+		importData();
+	} catch (e) {
+		newDomBoard();
+		newDataBoard();
+		startNewGame();
+	}
+}
+
+function onUndo() {
+	if (!prevData || beenUndone) return;
+	if (justReset) {
+		boardSizesIndex = prevData.boardSizesIndex;
+		newDomBoard();
+		turnsTaken = prevData.turnsTaken;
+	} else {
+		turnsTaken--;
+	}
+	victor = prevData.victor;
+	gboard = copy(prevData.gboard);
+	mboard = copy(prevData.mboard);
+	posPos = copy(prevData.posPos);
+	beenUndone = true;
+	justReset = false;
+	exportData(false);
+	wipe();
+	drawAll();
+	drawish();
+}
+
+function startNewGame() {
+	turnsTaken = 0;
+	posPos = [];
+	wipe();
+	freeMove();
+	drawish();
+}
+
+function storage(store, key, data) {
+	if (typeof data === "undefined") {
+		let value = store.getItem(key);
+		try {
+			return JSON.parse(value);
+		} catch (e) {
+			return value;
+		}
+	} else {
+		store.setItem(key, JSON.stringify(data));
+		return data;
+	}
+}
+
+// remove changable classes
+function wipe() {
+	$("#board, #board *").removeClass("x o cat x-ish o-ish nope");
+}
+
+// remove hover-higlight classes
+function wipeish() {
+	$(".g, .m").removeClass("x-ish o-ish nope");
+}
+
+
+
+function displayBoard(arr) {
+	let addCell = (cell) => {
+		if (cell === "cat") {
+			r += "="
+		} else {
+			r += cell;
+		}
+		r += " ";
+	}
+	let r = "Local Boards:\n";
+
+	for (let gy = 0; gy < getBoardSize(); gy++) {
+		for (let my = 0; my < getBoardSize(); my++) {
+			for (let gx = 0; gx < getBoardSize(); gx++) {
+				for (let mx = 0; mx < getBoardSize(); mx++) {
+					addCell(mboard[gx][gy][mx][my]);
+				}
+				if (gx < getBoardSize() - 1) {
+					r += "  ";
+				}
+			}
+			r += "\n";
+		}
+		r += "\n";
+	}
+	r += "\nGlobal Board:\n"
+
+	for (let gy = 0; gy < getBoardSize(); gy++) {
+		for (let gx = 0; gx < getBoardSize(); gx++) {
+			addCell(gboard[gx][gy]);
+		}
+		r += "\n";
+	}
+
+	r += "\n\nVictor:\n";
+	addCell(victor);
+
 	console.log(r);
 }
